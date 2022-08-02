46aa69df32ac4de011a08118ff55c0a8c344d51a
Iroro Orife <iorife@netflix.com>
2022-08-01 19:39:11 -0700

    [ADD] scripts for Aremu to try to fix M1 launch issue

---
+++ fixM1libsndfile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+bash -c "ln -s /opt/homebrew/lib/libsndfile.* /Library/Frameworks/Python.framework/Versions/3.9/lib/python3.9/site-packages/_soundfile_data/"
--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 # setup dirs
 mkdir -p ~/Desktop/audio-data
 
-python3 -m yoruba_voice_speech_recorder -p src/yoruba_voice_speech_recorder/prompts/timit.txt -d ~/Desktop/audio-data
+env DYLD_LIBRARY_PATH="/opt/homebrew/lib:$DYLD_LIBRARY_PATH" python3 -m yoruba_voice_speech_recorder -p src/yoruba_voice_speech_recorder/prompts/timit.txt -d ~/Desktop/audio-data
