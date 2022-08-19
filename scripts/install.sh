d719e97aec06dc5be16b9ca840a67fff533a6c66
Ruoho Ruotsi <ruoho.ruotsi@gmail.com>
2022-08-18 20:34:55 -0700

    [FIX] install to cleanly uninstall/install and suppress warnings to dev/null

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,14 +6,15 @@ mkdir -p ~/Desktop/audio-data
 
 # uninstall previous version
 echo "Uninstalling old YoVo Speech Recorder ..."
-pip3 uninstall yoruba-voice-speech-recorder
+yes | pip3 uninstall yoruba-voice-speech-recorder 2>/dev/null
 
 
-for i in {1..15}
+for i in {1..3}
 do
-   echo " "
+   sleep 1
+   echo "."
 done
 
 echo "Installing fresh YoVo Speech Recorder ..."
-CPPFLAGS="-I/opt/homebrew/include" LDFLAGS="-L/opt/homebrew/lib" pip3 install -e .
+CPPFLAGS="-I/opt/homebrew/include" LDFLAGS="-L/opt/homebrew/lib" pip3 install -e . 2>/dev/null
 
